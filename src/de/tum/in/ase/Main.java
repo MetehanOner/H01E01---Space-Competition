4ccf51fc515cf30cbd5c1fc79668679ff8066c6d
Tunar Mahmudov <tunar.mahmudov@mantaro.partners>
2022-10-03 19:17:21 +0400

    Create template exercise

---
+++ src/de/tum/in/ase/Main.java
@@ -0,0 +1,21 @@
+package de.tum.in.ase;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: 1. Print the welcome message
+
+        //TODO: 2. Ask and print first choice
+
+        //You can uncomment the following line and store each user input into a variable
+        //String input1 =
+
+        //TODO: 3. Ask and print second choice
+
+        //TODO: 4. Ask and print third choice
+
+        //TODO: 5. Print choices all together
+
+        //TODO: 6. Print the goodbye message
+
+    }
+}
