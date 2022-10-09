62efcfae1ce197ebf82e48841d0688fead2ade28
Tunar Mahmudov <tunar.mahmudov@tum.de>
2022-10-09 21:29:18 +0400

    Add comments

--- src/de/tum/in/ase/SpaceCompetition.java
+++ src/de/tum/in/ase/SpaceCompetition.java
@@ -4,16 +4,15 @@ public class SpaceCompetition {
     public static void main(String[] args) {
         //TODO: 1. Print the welcome message
 
-        //TODO: 2. Ask and print first choice
-
+        //TODO: 2. Ask the name of the first choice and print it on the console
         //You can uncomment the following line and store each user input into a variable
         //String input1 =
 
-        //TODO: 3. Ask and print second choice
+        //TODO: 3. Ask the name of the second choice and print it on the console
 
-        //TODO: 4. Ask and print third choice
+        //TODO: 4. Ask the name of the third choice and print it on the console
 
-        //TODO: 5. Print choices all together
+        //TODO: 5. Print out the order in which Eran and Jasper will compete for the competition
 
         //TODO: 6. Print the goodbye message
 
