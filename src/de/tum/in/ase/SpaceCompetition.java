0f2f593726103a4de5beb14175a7d71990f31d91
MetehanOner <a.metehanoner@gmail.com>
2022-10-19 23:25:58 +0200

    Fourth Delivery

--- src/de/tum/in/ase/SpaceCompetition.java
+++ src/de/tum/in/ase/SpaceCompetition.java
@@ -1,5 +1,5 @@
 package de.tum.in.ase;
-
+import java.util.*;
 
 public class SpaceCompetition {
     public static void main(String[] args) {
@@ -7,17 +7,17 @@ public class SpaceCompetition {
         System.out.println("Welcome Martians, We are Eran and Jasper from Earth!\n");
         //TODO: 2. Ask the name of the first choice and print it on the console
         //You can uncomment the following line and store each user input into a variable
-        //Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("What is your first choice?");
-        String input1 = "parkrun";
+        String input1 = in.nextLine();
         System.out.println("First choice: " + input1);
         //TODO: 3. Ask the name of the second choice and print it on the console
         System.out.print("What is your second choice?");
-        String input2 = "swimming";
+        String input2 = in.nextLine();
         System.out.println("Second choice: " + input2);
         //TODO: 4. Ask the name of the third choice and print it on the console
         System.out.print("What is your third choice?");
-        String input3 = "pole vault";
+        String input3 = in.nextLine();
         System.out.println("Third choice: " + input3);
         //TODO: 5. Print out the order in which Eran and Jasper will compete for the competition
         System.out.println("Jasper and Eran will start with " + input1 + ", continue with " + input2 + ", and end with " + input3 + ".");
